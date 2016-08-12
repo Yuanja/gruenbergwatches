d212ebf0bfbe6851f80d87cd3edd730507aabd5f
James <jyuan@salesforce.com>
2016-08-11 21:22:48 -0700

    Sorting fix for new arrival

--- upload/catalog/controller/product/newarrivals.php
+++ upload/catalog/controller/product/newarrivals.php
@@ -18,7 +18,7 @@ class ControllerProductNewarrivals extends Controller {
 		if (isset($this->request->get['order'])) {
 			$order = $this->request->get['order'];
 		} else {
-			$order = 'ASC';
+			$order = 'DESC';
 		}
 
 		if (isset($this->request->get['page'])) {
